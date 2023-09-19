52d7164b79a90746de33c82b1746a9134d11e5be
tajirri <clarencewagah@gmail.com>
2023-09-19 09:46:05 -0700

    task 0

--- _putchar.c
+++ _putchar.c
@@ -1,16 +1,10 @@
-#include "main.h"
+#include <unistd.h>
 /**
- * _putchar - prints character
- *
- * desc: character is being printed to standard output
- *
- * @c: character to be printed
- *
- * Return: the number of characters printed to standard output
- * i.e 1
+ * _putchar - write character c to the stdout
+ * @c: the character
+ * Return: 1 on success and -1 otherwise
  */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
