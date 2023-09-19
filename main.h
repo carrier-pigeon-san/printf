b838bfba0bc2732b234da5b61ed77c5e72536c80
tajirri <clarencewagah@gmail.com>
2023-09-19 10:47:02 -0700

    Do hard things.

--- main.h
+++ main.h
@@ -1,57 +1,118 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/* The libraries that we'll need */
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 /**
- * struct specifiers - A specifier struct.
+ * struct fmt - Struct op
  *
- * @specifier: A character to indicate which specifier this is.
- * @f: Pointer to a it's respective print function.
+ * @fmt: The format.
+ * @fn: The function associated.
  */
-typedef struct specifiers
+struct fmt
 {
-	char *specifier;
-	int (*f)(va_list args);
-} spec_t;
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
-/* Our printers */
-int _putchar(char c);
-int _puts(char *s);
 
-/* c-s-i-d_printers.c modules */
-int _print_char(va_list args);
-int _print_string(va_list args);
-int _print_int(va_list args);
-void _integer_recursion(int integer);
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
-/* b-u_printers.c modules */
-int _print_binary(va_list args);
-void _binary_recursion(int integer);
-int _print_unsigned(va_list args);
-void _unsigned_recursion(int integer);
-
-/* o_printers.c modules */
-int _print_octal(va_list args);
-void _octal_recursion(int integer);
-
-/* x-X_printers.c modules */
-int _print_hex(va_list args);
-int _print_HEX(va_list args);
-void _hex_recursion(int integer);
-void _HEX_recursion(int integer);
-
-/* print_format.c module */
-/* The function that'll determine which specifer to print */
-int _print_format(const char *format, va_list args);
-int isValidSpecifier(char type);
-int _print_specifier(char format, va_list args);
-
-/* The _printf function */
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
-#endif
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
 
